5acc100f1ed3a941e5482af54643c40dbab6174a
Alexandre Henrique Lopes <lopesah@gmail.com>
2023-12-18 15:28:34 -0300

    Atualização 18-12-2023 v3

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,3 +1,4 @@
+const converteIds = require('../utils/conversorDeStringHelper.js');
 class Controller {
   constructor(entidadeService) {
     this.entidadeService = entidadeService;
@@ -22,6 +23,17 @@ class Controller {
     }
   }
 
+  async pegaUm(req, res) {
+    const { ...params } = req.params;
+    const where = converteIds(params);
+    try {
+      const umRegistro = await this.entidadeService.pegaUmRegistro(where);
+      return res.status(200).json(umRegistro);
+    } catch (erro) {
+      return res.status(500).json({ erro: erro.message });
+    }
+  }
+
   async criaNovo(req, res) {
     const dadosParaCriacao = req.body;
     try {
@@ -33,11 +45,12 @@ class Controller {
   }
 
   async atualiza(req, res) {
-    const { id } = req.params;
+    const { ...params } = req.params;
     const dadosAtualizados = req.body;
+    const where = converteIds(params);
     try {
       //isUpdated
-      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, where);
       if (!foiAtualizado) {
         return res.status(400).json({ mensagem: 'registro não foi atualizado' });
       }
@@ -48,10 +61,11 @@ class Controller {
   }
 
   async exclui(req, res) {
-    const { id } = req.params;
+    const { ...params } = req.params;
+    const where = converteIds(params);
     try {
-      await this.entidadeService.excluiRegistro(Number(id));
-      return res.status(200).json({ mensagem: `id ${id} deletado` });
+      await this.entidadeService.excluiRegistro(where);
+      return res.status(200).json({ mensagem: 'Registro deletado' });
     } catch (erro) {
       return res.status(500).json({ erro: erro.message });
     }
